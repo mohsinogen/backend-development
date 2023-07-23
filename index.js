39c7b524072dfe241c324ad2e5fba33925aef168
Mohsin Ansari <mohsinogen@gmail.com>
2023-07-23 16:43:01 +0530

    session 2 changes updated

--- index.js
+++ index.js
@@ -1,126 +1,28 @@
 // importing modules
 const http = require("http");
-const url = require("url");
-const helper = require("./helper");
+
+// Importing controllers
+const homeController = require("./controllers/homeController");
+const userController = require("./controllers/userController");
 
 // creating a http server
 const server = http.createServer((req, res) => {
   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
 
   if (req.url == "/") {
-    // returning a success response when client hits home route
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(
-      JSON.stringify({
-        code: 200,
-        remark: "success",
-      })
-    );
+    homeController.home(req, res);
   } else if (req.url.includes("/users")) {
-    // when client hits /users path
     try {
-      // reading the stored data from file
-      const storedData = helper.read();
-
       if (req.method == "GET") {
-        res.writeHead(200, { "Content-Type": "application/json" });
-
-        // checking if name is present in query parameter
-        const parsedUrl = url.parse(req.url, true);
-        if (parsedUrl.query.name) {
-          const result = storedData.filter((item) =>
-            item.name.toLowerCase().includes(parsedUrl.query.name.toLowerCase())
-          );
-          res.end(JSON.stringify(result));
-        } else {
-          res.end(JSON.stringify(storedData));
-        }
+        userController.getUsers(req, res);
       } else if (req.method == "POST") {
-        // reading data from rquest body
-        let body = "";
-        req.on("data", (chunk) => {
-          body += chunk;
-        });
-
-        req.on("end", () => {
-          const { name, email } = JSON.parse(body);
-          // updating the data
-          const newUserID =
-            storedData.length > 1
-              ? storedData[storedData.length - 1]["id"] + 1
-              : 1;
-          const updatedData = [...storedData, { id: newUserID, name, email }];
-          helper.write(updatedData);
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({
-              code: 200,
-              remark: "User added successfully",
-              data: null,
-            })
-          );
-        });
+        userController.createUser(req, res);
       } else if (req.method == "PUT") {
-        // reading data from rquest body
-        let body = "";
-        req.on("data", (chunk) => {
-          body += chunk;
-        });
-
-        req.on("end", () => {
-          const { id, name, email } = JSON.parse(body);
-
-          // searching and editing the particular user
-          const userToEditIndex = storedData.findIndex(
-            (item) => item.id === id
-          );
-          storedData[userToEditIndex] = {
-            ...storedData[userToEditIndex],
-            name,
-            email,
-          };
-
-          helper.write(storedData);
-          res.writeHead(200, { "Content-Type": "application/json" });
-          res.end(
-            JSON.stringify({
-              code: 200,
-              remark: "User updated successfully",
-              data: null,
-            })
-          );
-        });
+        userController.updateUser(req, res);
       } else if (req.method == "DELETE") {
-        // accessing the user id from query
-        const parsedUrl = url.parse(req.url, true);
-        const userToDeleteIndex = storedData.findIndex(
-          (item) => item.id === Number(parsedUrl.query.id)
-        );
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-
-        if (userToDeleteIndex != -1) {
-          storedData.splice(userToDeleteIndex, 1);
-          helper.write(storedData);
-
-          res.end(
-            JSON.stringify({
-              code: 200,
-              remark: "User deleted successfully",
-              data: null,
-            })
-          );
-        } else {
-          res.end(
-            JSON.stringify({
-              code: 400,
-              remark: "User not found",
-              data: null,
-            })
-          );
-        }
+        userController.deleteUser(req, res);
       } else {
-        // returning error if method is other than GET and POST
+        // returning error if method is other than GET, POST, PUT, DELETE
         res.writeHead(405, { "Content-Type": "application/json" });
         res.end(
           JSON.stringify({
@@ -131,6 +33,7 @@ const server = http.createServer((req, res) => {
         );
       }
     } catch (e) {
+      // returning error if something wents wrong on server
       console.log(e);
       res.writeHead(500, { "Content-Type": "application/json" });
       res.end(
@@ -143,7 +46,7 @@ const server = http.createServer((req, res) => {
       );
     }
   } else {
-    // returning a not found error when client hits any url other than above two
+    // returning a not found error when client hits any url other than "/" and "/users"
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(
       JSON.stringify({

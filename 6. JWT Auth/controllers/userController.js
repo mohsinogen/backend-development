c6c5d3db05ab970945e326125b87878dc5245a52
mohsin ansari <mohsinogen@gmail.com>
2023-08-26 10:41:22 +0000

    JWT auth changes

---
+++ 6. JWT Auth/controllers/userController.js
@@ -0,0 +1,13 @@
+import User from "../models/userModel.js";
+import asyncHandler from "express-async-handler";
+
+const registerUser = asyncHandler(async (req, res) => {
+
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+
+});
+
+
+export { registerUser, loginUser };
---
+++ 6. JWT Auth/index.js
@@ -0,0 +1,40 @@
+// importing packages
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import cors from "cors"
+import morgan from "morgan";
+//
+import connectDB from "./config/db.js";
+import { errorHandler, notFound } from "./middlewares/errorMiddleware.js";
+
+// Importing Routes
+import userRoutes from "./routes/userRoutes.js"
+
+
+dotenv.config();
+//connectDB();
+
+// express configs
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'))
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    code: 200,
+    remark: "success",
+  });
+});
+
+app.use("/users",userRoutes)
+
+// Middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`.yellow.underline);
+});
